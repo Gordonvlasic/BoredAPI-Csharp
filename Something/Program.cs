b495baca5c94d7ebc1d30fec414a4815cd150d4f
gordonvlasic <gvlasic@my.centennialcollege.ca>
2022-10-03 17:10:21 -0400

    updated

--- Something/Program.cs
+++ Something/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http;
+using Newtonsoft.Json;
 
+class Program
+{
+    HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args)
+    {
+        Program program = new Program();
+        await program.GetTodoItems();
+    }
+
+    private async Task GetTodoItems()
+    {
+        string response = await client.GetStringAsync(
+            "https://www.boredapi.com/api/activity");
+
+        Todo todo = JsonConvert.DeserializeObject<Todo>(response);
+
+        Console.WriteLine(todo);
+    }
+
+    public class Todo
+    {
+        public string activity { get; set; } = string.Empty;
+        public string type { get; set; } = string.Empty;
+        public int participants { get; set; }
+        public double price { get; set; }
+        public string link { get; set; } = string.Empty;
+        public string key { get; set; } = string.Empty;
+        public double accessibility { get; set; }
+
+        public override string ToString()
+        {
+            return $"The activity that you should consider doing: {activity}.\nIt requires {participants} person(s)." +
+                $"\nThe type of activity: {type}.";
+        }
+    }
+}
